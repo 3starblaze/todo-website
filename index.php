654c53c96e28da069b21c69fbfd9c3a08bd2561d
Deniss Fjodorovs <3starblaze@gmail.com>
2019-06-15 11:24:10 +0300

    Link script to html

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <link href="styles/style.css" rel="stylesheet">
+    <script src="checkbox-listener.js"></script>
     <meta charset="utf-8">
     <title>Darāmo lietu saraksts</title>
   </head>
