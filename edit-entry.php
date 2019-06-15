892038f04df8e52521286a71a32ab04a0c28df16
Deniss Fjodorovs <3starblaze@gmail.com>
2019-06-15 08:22:44 +0300

    Add base html for edit entry page

---
+++ edit-entry.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+  <head>
+    <link href="styles/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>Pievienot jaunu</title>
+  </head>
+  <body>
+    <h1>Darāmo lietu saraksts</h1>
+    <form action="" method="post">
+      <label for="title">Virsraksts:</label>
+      <input type="text" id="title" name="title" value="">
+      <label for="description">Apraksts:</label>
+      <input type="text" id="description" name="description">
+      <button>Saglabāt</button>
+    </form>
+       
+  </body>
+</html>
+
+<?php
+
+?>
