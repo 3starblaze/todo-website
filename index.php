2cd768b5011c1b645d9a3e398f33c4729fcbf3bf
Deniss Fjodorovs <3starblaze@gmail.com>
2019-06-16 11:25:44 +0300

    Add some bootstrap classes for styling

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
   </head>
   <body>
     <h1>Darāmo lietu saraksts</h1>
+      <!-- Wrap all tasks in "row" div to make tasks stack horizontally-->
+    <div class="">
     <?php
 
 $servername = "localhost";
@@ -33,6 +35,7 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM todolist";
 $result = $conn->query($sql);
 
+
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -45,31 +48,40 @@ if ($result->num_rows > 0) {
         $date = htmlEntities($row['date'], ENT_QUOTES);
         $description = htmlEntities($row['description'], ENT_QUOTES);
                
-        echo "<div id=$row[id] class=\"task\">".
-               "<form action=\"status-editor.php\" method=\"post\">".
-                 "<button class=\"check\" name=\"id\" ".
-                 "value=$row[id]>".
-                 "<input type=\"checkbox\"".
-                   $checked_string.
-                  "/>".
-                  "</button>".
-               "</form>".
+        echo "<div id=$row[id] class=\"task jumbotron \">".
+               "<ul class=\"nav\">".
+                 "<li class=\"left-side\">".
+                 "<form action=\"status-editor.php\" method=\"post\" ".
+                 "class=\"checkform\">".
+                   "<button class=\"check\" name=\"id\" ".
+                   "value=$row[id]>".
+                   "<input type=\"checkbox\"".
+                     $checked_string.
+                    "/>".
+                    "</button>".
+                 "</form>".
                "<h2 class=\"title\">$title</h2>".
+                "</li>".
+                "<li class=\"right-side\">".
                 "<p class=\"date\">$date</p>".
+                "</li>".
+                "</ul>".
                 "<p class=\"description\">$description</p>".
                 "<form action=\"edit-entry.php\" method=\"post\">".
-                   "<button name=\"id\" value=$row[id]>Labot</button>".
+                   "<button name=\"id\" ".
+                   "class=\"edit btn btn-outline-secondary\"".
+                   " value=$row[id]>Labot</button>".
                 "</form>".
               "</div>";
-        //TODO: link to todo task editing page
     }
 }
 
 $conn->close();
 ?>
-
+    </div>
     <a href="create-new.html">
-      <button id="create-new">Pievienot jaunu</button>
+      <button id="create-new" class="btn btn-primary btn-lg" >
+    Pievienot jaunu</button>
     </a>
   </body>
 </html>
